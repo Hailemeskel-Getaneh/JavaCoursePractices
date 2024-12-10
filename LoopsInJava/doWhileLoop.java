edca2c627216f26a0fd3dc85c034372a324e6415
Hailemeskel Getaneh <hailegetaneh1221@gmail.com>
2024-12-10 17:42:29 +0300

    loops in java

---
+++ LoopsInJava/doWhileLoop.java
@@ -0,0 +1,19 @@
+package LoopsInJava;
+
+public class doWhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 0;
+		
+		do {
+			
+			System.out.println(x);
+			x++;
+		}
+		while(x <= 10);
+
+	}
+
+}
